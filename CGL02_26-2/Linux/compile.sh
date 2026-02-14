504cb8a1674ebd0293da51f515b5a3a33ed6b5ac
David <anddavbauney@outlook.com>
2026-02-14 16:16:51 -0600

    Pequeños cambios para hacerlos compilar en Linux

--- CGL02_26-2/Linux/compile.sh
+++ CGL02_26-2/Linux/compile.sh
@@ -1,3 +1,4 @@
 #Dependencias requeridas, g++, GLEW y glfw
+#Recomendación de utilizar X11 o en su defecto Wayland con un GLEW versión 2.2.0 o inferior y glfw versión 3.3.8 o inferior.
 
 g++ mainbase.cpp -lGLEW -lGL -lglfw -o main.out
