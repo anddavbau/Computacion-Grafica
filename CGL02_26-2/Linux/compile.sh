dde85658a0960cefa5cdd9ef86aa9bd1a3a7bb4d
David <anddavbauney@outlook.com>
2026-02-16 18:05:19 -0600

    Modificación al script para hacerlo mas comodo

--- CGL02_26-2/Linux/compile.sh
+++ CGL02_26-2/Linux/compile.sh
@@ -1,4 +1,7 @@
 #Dependencias requeridas, g++, GLEW y glfw
-#Recomendación de utilizar X11 o en su defecto Wayland con un GLEW versión 2.2.0 o inferior y glfw versión 3.3.8 o inferior.
+#Recomendación de utilizar X11 o en su defecto Wayland con GLEW versión 2.2.0 o inferior y glfw versión 3.3.8 o inferior.
 
-g++ mainbase.cpp -lGLEW -lGL -lglfw -o main.out
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FILE_NAME=mainbase
+
+g++ "$SCRIPT_DIR/$FILE_NAME.cpp" -lGLEW -lGL -lglfw -o "$SCRIPT_DIR/$FILE_NAME.out"
